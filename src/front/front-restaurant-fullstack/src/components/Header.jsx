058d2e866169cbc11528afc3addc7cb6a7dbe276
GabrielDRamos <gduarter21@gmail.com>
2025-01-13 00:06:44 -0300

    deletando o front

--- src/front/front-restaurant-fullstack/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Header.module.css";
-
-export function Header() {
-  return (
-    <header>
-      <strong className={styles.header}>Restaurantes Maia</strong>
-    </header>
-  );
-}
